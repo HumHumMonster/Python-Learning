24cb2cd83b7063b422b6c8aebf76c4669d7cdc8a
ZhangHuanxin <40661210+HumHumMonster@users.noreply.github.com>
2019-10-04 11:51:00 +0800

    Update hm爬虫.py

--- hm爬虫.py
+++ hm爬虫.py
@@ -10,24 +10,35 @@ URL = 'https://www.simply-hentai.com/tag/color/page/'
 for i in range (1 , 50) :
     #第几页的网址
     url = URL + str(i)
-    # print(url)
     text = requests.get(url).text
 
     mangas_urls = re.findall('<a class="js-overview-link" href="(.*?)">', text)
     mangas_names = re.findall('<a class="js-overview-link" href=".*?">(.*?)</a></h3>', text)
-    #print(mangas_names)
+
     for j in range (0 , len(mangas_urls)) :
-        #print(mangas_names[j])
+
         mangas_url = mangas_urls[j] + '/all-pages'
-        #print(mangas_url)
+
         mangas_name = re.sub('[?|><*/]', '', mangas_names[j])
-        #print(mangas_name)
-        #os.makedirs('D:/爬虫计划/%s' % mangas_name)
-        print(mangas_url)
+
+        print(mangas_name)
+
+        if os.path.exists('D:/爬虫计划/%s' % mangas_name) :
+            print("已经有了，跳过")
+            continue
+
+        os.makedirs('D:/爬虫计划/%s' % mangas_name)
+
+        #print(mangas_url)
+
         mangas_text = requests.get(mangas_url).text
-        print(mangas_text)
+
+        #print(mangas_text)
+
         pic_urls = re.findall('"path":"(.*?)"' , mangas_text)
+
         #print(pic_urls)
+
         cnt = 1
         for i in pic_urls :
             print(i)
@@ -35,9 +46,9 @@ for i in range (1 , 50) :
             pic_name = str(cnt) + '.jpg'
             cnt += 1
             pic_url = re.findall('&quot;full&quot;:&quot;(.*?)&quot;', pic_text)
+            if len(pic_url) == 0 :
+                continue
             end_url = pic_url[0]
-            #print(end_url)
-            #print(end_url)
             image = requests.get(end_url).content
             with open(f'D:/爬虫计划/{mangas_name}/%s' % pic_name , 'wb') as file:
                     file.write(image)
